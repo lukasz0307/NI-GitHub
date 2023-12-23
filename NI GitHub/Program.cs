696eb8c050a4ce0b73a26460ec992c23f4b4cc13
aniao <aniao@LAPTOP-OC2DI8K7>
2023-12-23 16:04:05 +0100

    Dodaj pliki projektów.

---
+++ NI GitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj swoje imię");
+        string imie = Console.ReadLine();
+        Console.WriteLine("Podaj swoje Nazwisko");
+        string nazw = Console.ReadLine();
+        Console.WriteLine("Podaj swój wiek");
+        string wiek = Console.ReadLine();
+        Console.WriteLine($"Witaj {imie} {nazw} lat {wiek}");
+    }
+}
